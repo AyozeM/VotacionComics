a1ff118c6b550945c19a0e24ba2c5f27ce2a9281
Ayoze <ayozemartinhdez@gmail.com>
2018-02-06 21:59:39 +0000

    realizada la portada de la aplicacion

---
+++ src/js/constants.js
@@ -0,0 +1,8 @@
+export const marvelApi = {
+    url:"https://gateway.marvel.com:443",
+    key:"7f1e00c4792e7da5579f0b087a4c7d7f",
+    methods:{
+        comics:"/v1/public/comics",
+        characters:"/v1/public/characters"
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,63 @@
 import $ from 'jquery';
+import {marvelApi} from './constants';
+$(document).ready(()=>{
+    getLastComics();
+    getCharacters();
+    $("section").find("div").on("click","div",e=>{
+        alert($(e.currentTarget).data("id"));
+    });
+    $("#search").click(e=>{
+        alert($(e.currentTarget).siblings("inupt").val());
+    });
+});
+const getCharacters = () =>{
+    $.ajax({
+        url:`${marvelApi.url}${marvelApi.methods.characters}`,
+        success:response=>{
+            $(".characters").find(".preloader").remove();
+            response.data.results.map(e=>{
+                createHTML({name:e.name,img:`${e.thumbnail.path}.${e.thumbnail.extension}`,id:e.id}).appendTo(".characters");
+            })
+        },
+        data:{apikey:marvelApi.key,limit:5},
+        error:()=>{
+            alert("Hubo un fallo");
+        },
+        beforeSend:()=>{
+            $(".characters").append(
+                createPreloader("Cargando personajes...")
+            )
+        }
+    });
+};
+const createHTML = data =>
+    $("<div>",{class:'item','data-id':data.id}).append(
+        $(`<p>${data.name}</p>`)
+    ).append(
+        $("<img>",{src:`${data.img}`})
+    );
+
+const createPreloader = text=>$(`<p class="fas fa-spinner fa-pulse preloader">${text}</p>`)
+/**
+ * Obtiene e inserta 5 comics de la ultima semana
+ */
+const getLastComics = () =>{
+    $.ajax({
+        url:`${marvelApi.url}${marvelApi.methods.comics}`,
+        success:response=>{
+            $(".comics").find(".preloader").remove();
+            response.data.results.map(e=>{
+                createHTML({name:e.title,img:`${e.thumbnail.path}.${e.thumbnail.extension}`,id:e.id}).appendTo("section .comics");
+            });
+        },
+        beforeSend:()=>{
+            $(".comics").append(
+                createPreloader("Cargando comics...")
+            )
+        },
+        data:{apikey:marvelApi.key,limit:5,dateDescriptor:"lastWeek"},
+        error:()=>{
+            alert("hubo un error");
+        }
+    });
+};
